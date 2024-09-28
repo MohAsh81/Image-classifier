33513ffa54efe1de39481d8b13ffd5fd21a284d6
Mohammad <mohammad.m.osati@gmail.com>
2024-09-28 21:33:58 +0330

    file path edited and picture updated.

--- model_classifier.py
+++ model_classifier.py
@@ -6,7 +6,7 @@ import cv2
 import matplotlib
 
 img = cv2.imread(
-    "Projects\Image-classifier\Pictures\Maria Sharapova\maria.jpg")
+    "Projects\Image-classifier\Pictures\Maria_Sharapova\maria.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 face_cascade = cv2.CascadeClassifier(
@@ -48,3 +48,30 @@ for entry in os.scandir(path_to_data):
 if os.path.exists(path_to_cr_data):
     shutil.rmtree(path_to_cr_data)
 os.mkdir(path_to_cr_data)
+
+
+cropped_image_dirs = []
+celebrity_file_names_dict = {}
+
+for img_dir in img_dirs:
+    count = 1
+    celebrity_name = img_dir.split("\\")[-1]
+    print(celebrity_name)
+
+    celebrity_file_names_dict[celebrity_name] = []
+
+    for entry in os.scandir(img_dir):
+        roi_color = get_cropped_image_if_2_eyes(entry.path)
+        if roi_color is not None:
+            cropped_folder = path_to_cr_data + celebrity_name
+            if not os.path.exists(cropped_folder):
+                os.makedirs(cropped_folder)
+                cropped_image_dirs.append(cropped_folder)
+                print("Generating cropped images in folder: ", cropped_folder)
+
+            cropped_file_name = celebrity_name + str(count) + ".png"
+            cropped_file_path = cropped_folder + "/" + cropped_file_name
+
+            cv2.imwrite(cropped_file_path, roi_color)
+            celebrity_file_names_dict[celebrity_name].append(cropped_file_path)
+            count += 1
