374157b0147616697cfdb0f540ebf166ce5424a3
Mohammad <mohammad.m.osati@gmail.com>
2024-09-29 14:28:54 +0330

    Hyper tuning and pipeline

--- model_classifier.py
+++ model_classifier.py
@@ -1,3 +1,9 @@
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
 from sklearn.metrics import classification_report
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
@@ -134,4 +140,42 @@ pipe = Pipeline([('scaler', StandardScaler()),
                 ('svc', SVC(kernel='rbf', C=10))])
 pipe.fit(X_train, y_train)
 
-print(classification_report(y_test, pipe.predict(X_test)))
+model_params = {
+    'svm': {
+        'model': svm.SVC(gamma='auto', probability=True),
+        'params': {
+            'svc__C': [1, 10, 100, 1000],
+            'svc__kernel': ['rbf', 'linear']
+        }
+    },
+    'random_forest': {
+        'model': RandomForestClassifier(),
+        'params': {
+            'randomforestclassifier__n_estimators': [1, 5, 10]
+        }
+    },
+    'logistic_regression': {
+        'model': LogisticRegression(solver='liblinear', multi_class='auto'),
+        'params': {
+            'logisticregression__C': [1, 5, 10]
+        }
+    }
+}
+
+
+scores = []
+best_estimators = {}
+for algo, mp in model_params.items():
+    pipe = make_pipeline(StandardScaler(), mp['model'])
+    clf = GridSearchCV(pipe, mp['params'], cv=5, return_train_score=False)
+    clf.fit(X_train, y_train)
+    scores.append({
+        'model': algo,
+        'best_score': clf.best_score_,
+        'best_params': clf.best_params_
+    })
+    best_estimators[algo] = clf.best_estimator_
+
+df = pd.DataFrame(scores, columns=['model', 'best_score', 'best_params'])
+
+print(df)
